3c0f20e730653a4991c7e8494e3bedeb2056ed7c
Daniyar <kaliyev.daniyar@gmail.com>
2016-05-20 08:02:11 +0100

    First commit

---
+++ FileWatcher/FileWatcher/FileWatcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    public class FileWatcher
+    {
+        List<string> _directories;
+        List<FileSystemWatcher> _watchers = new List<FileSystemWatcher>();
+
+        public delegate void OnChange(object sender, FileSystemEventArgs args);
+
+        private OnChange OnChangeEnt;
+
+        public FileWatcher(List<string> dirsToWatch, string filter, OnChange onChange)
+        {
+            OnChangeEnt = onChange;
+
+            foreach (var dir in dirsToWatch)
+            {
+                var watcher = new FileSystemWatcher(dir);
+                watcher.Filter = filter;
+                watcher.Changed += new FileSystemEventHandler(onChange);
+                watcher.Created += new FileSystemEventHandler(onChange);
+                watcher.Renamed += new RenamedEventHandler(OnChangeHandler);
+                watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.LastAccess | NotifyFilters.FileName;
+                watcher.IncludeSubdirectories = true;
+                watcher.EnableRaisingEvents = true;
+                _watchers.Add(watcher);
+            }
+        }
+
+        private void OnChangeHandler(object sender, FileSystemEventArgs args)
+        {
+            this.OnChangeEnt(sender, args);
+        }
+    }
+}
